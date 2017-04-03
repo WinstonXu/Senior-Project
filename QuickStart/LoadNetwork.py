102a79d99fd9de01251b4967f589693b262ac1ed
nabrowning <nattimwin@cccs.cs.coloradocollege.edu>
2017-04-03 16:10:57 -0600

    Messed up, lost Natlalie's work but LoadNetwork works

--- QuickStart/LoadNetwork.py
+++ QuickStart/LoadNetwork.py
@@ -4,25 +4,26 @@ import os
 import numpy as np
 import sys
 
-
 def cannedResponse():
-    return 'i_like_booty'
+    return "I like booty"
 
 def testmodel(file_name):
-    print(os.path.dirname(__file__))
+    print os.path.dirname(__file__)
     os.chdir(os.path.curdir)
     model = load_model('pic_model.h5')
     if not os.path.exists(os.path.join(os.path.dirname(__file__), "Uploads", file_name)):
-        print("Image does not exist")
+        print "Image does not exist"
     else:
         im = Image.open(os.path.join(os.path.dirname(__file__), "Uploads", file_name))
-        im = im.resize((84,28))
+        im = im.resize((28,84))
         im = im.convert('L')
         # newim = ImageEnhance.Contrast(im)
         # newim = newim.enhance(2.5).save()
         pixelData = im.getdata()
-        pixelData = np.asarray(pixelData, dtype=np.float64).reshape((1, im.size[0], im.size[1], 1))
-        pixelData *= 1 / 255.
+        pixelData = np.asarray(pixelData, dtype=np.float64).reshape((1, 1,  im.size[0], im.size[1]))
+        # pixelData *= 1 / 255.
+        newim = Image.fromarray(pixelData[0][0], 'L')
+        newim.save("testim.png")
         res = model.predict(pixelData)
         # print res.shape
         equation = ""
@@ -37,9 +38,7 @@ def testmodel(file_name):
         if operator == 3:
             equation += "/"
         equation += "%d" %(np.argmax(res[0][14:]))
-        print('equation: ' + equation)
         return equation
 
 if __name__ == "__main__":
-    testmodel(sys.argv[1])
-
+    print testmodel(sys.argv[1])
